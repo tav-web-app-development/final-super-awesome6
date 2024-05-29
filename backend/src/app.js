aa6c052fc1ce2e73703c93530f45ee2f77cc21fd
Malav S Patel <malavpatel2607775@gmail.com>
2024-05-28 23:25:08 -0400

    Backend express setup and depencies added

---
+++ backend/src/app.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+//Imports
+import express from "express";
+import cors from "cors";
+import pkg from "body-parser";
+const { json, urlencoded } = pkg;
+
+// Create and configure
+const app = express();
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cors());
+
+//Routes
+
+//Root Router
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Main Page</h1>");
+});
+
+app.get("/error", (req, res) => {
+  throw new Error("Something has gone terribly wrong...");
+});
+
+// Error Handling
+// Unknown Request handler
+app.all("*", (req, res) => {
+  res.status(404).json({
+    msg: "Something is wrong try again later ",
+    reqBody: req.body,
+    reqPath: req.path,
+    reqQuery: req.query,
+    reqMethod: req.method,
+    statusCode: res.statusCode,
+  });
+});
+
+// Internal Error handler
+const handleError = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Malfunction");
+};
+
+app.use(handleError);
+
+export default app;
