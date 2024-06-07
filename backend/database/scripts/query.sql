5dbeac49894b9a82d1ec529bf1b5c16ea6c4d6b9
Malav S Patel <malavpatel2607775@gmail.com>
2024-06-06 21:56:14 -0400

    added tables query

--- backend/database/scripts/query.sql
+++ backend/database/scripts/query.sql
@@ -1,24 +1,45 @@
-DROP TABLE IF EXISTS Admin ;
-DROP TABLE IF EXISTS  Employee;
-DROP TABLE IF EXISTS Department ;
+DROP TABLE IF EXISTS Admin;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Department;
 
 CREATE TABLE IF NOT EXISTS Admin(
-    id INTEGER PRIMARY KEY ,
-    email VARCHAR NOT NULL UNIQUE, 
-    password VARCHAR NOT NULL UNIQUE
+    id SERIAL PRIMARY KEY ,
+    email VARCHAR(50) NOT NULL UNIQUE, 
+    password VARCHAR(140) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Department(
-    id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR NOT NULL UNIQUE
+    id  SERIAL	 PRIMARY KEY ,
+    name VARCHAR(30) NOT NULL UNIQUE
 );
 
-CREATE TABLE Employee(
-    id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR  NOT NULL,
-    email VARCHAR NOT NULL UNIQUE,
-    password VARCHAR NOT NULL UNIQUE,
-    salary INTEGER NOT NULL,
-    dept_id INTEGER NOT NULL,
-    FOREIGN KEY (dept_id) REFERENCES Department(id) 
+CREATE TABLE IF NOT EXISTS Employee(
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(40) NOT NULL UNIQUE,
+    password VARCHAR(150) NOT NULL UNIQUE,
+    salary INT NOT NULL,
+    dept_id INT NOT NULL,
+    FOREIGN KEY (dept_id) REFERENCES Department(id)
 );
+
+-- Insert data into the Admin table
+INSERT INTO Admin (email, password) 
+VALUES
+('a@a.com', 'aaa'),
+('b@b.com', 'bbb'),
+('c@c.com', 'ccc');
+
+-- Insert data into the Department table
+INSERT INTO Department (name) 
+VALUES
+('IT'),
+('MARKETING');
+
+-- Insert data into the Employee table
+INSERT INTO Employee (name, email, password, salary, dept_id)
+VALUES
+('aaa', 'a@a.com', 'aaa', 6000000, 1),
+('bbb', 'b@b.com', 'bbb', 300000, 2),
+('ccc', 'c@c.com', 'ccc', 4000000, 1);
+
